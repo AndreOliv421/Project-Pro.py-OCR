8be49beea6ace8aa1e20614bef0c69d0daf98090
AndreOliv421 <andreoliv421@gmail.com>
2025-08-14 21:26:56 -0300

    Update ocr_pro.py

--- ocr_pro.py
+++ ocr_pro.py
@@ -15,7 +15,28 @@ import re
     translator = Translator ()
     
 def inches_to_mm(text):
+        
         """in. to mm converter in. text"""
     pattern = r'(\d+(\.\d+?)\s?(in|inch|inches|")\b'
 def replace(match):
-    inches=
+    inches = float(match.group(1))):
+    mm = inches*25.4
+    return f"{mm:.2f}mm"
+    return re.sub(pattern, replace, text)
+    
+            # Open to PDF
+    pdf_DOcument=fitz.open(pdf_path)
+    
+    for page_number in range(len(pdf_document[page_number]
+           
+            # Extraction of txt. blocks
+    text_blocks=page.get_text("blocks")
+    
+    if not text_blocks: 
+        
+            #  OCR if there is no text
+    pix = page.get_pixmap()
+    img = image.open(io.BytesIO(pix.tobytes()))
+    text = pytesseract.image_to_string(img, lang='eng')
+    text = inches_to_mm(text)
+        
